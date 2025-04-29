726117875cebda257bc28178fc1130429e76c2df
vimalraj <mohanvimal042002@gmail.com>
2025-04-29 21:43:19 +0530

    29/04

---
+++ Forloop.java
@@ -0,0 +1,16 @@
+public class Forloop
+{
+public static void main(String[]args){
+int row=1;
+while(row<=3){
+//public static void main(String[]args){
+for(int col=1;col<=5;col++)
+{
+System.out.print(col + " ");
+}
+
+System.out.println();
+row++;
+}
+}
+}
