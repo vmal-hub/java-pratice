136928950eb94069877a7368de7be321a3cdc0ab
vimalraj <mohanvimal042002@gmail.com>
2025-05-02 22:29:52 +0530

    pattern

---
+++ Pattern.java
@@ -0,0 +1,16 @@
+public class Pattern{
+
+public static void main (String[]args){
+
+for(int row=1; row<=5; row++)
+{
+  for(int col=1; col<=row; col++){
+
+ System.out.print(5-row+col+ " ");
+}                   
+
+System.out.println();
+}
+}
+
+}
