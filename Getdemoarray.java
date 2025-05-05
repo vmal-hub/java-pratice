59965ec1a1d676f5edba2a66cf7b98031265713d
vimalraj <mohanvimal042002@gmail.com>
2025-05-05 21:53:00 +0530

    arr

---
+++ Getdemoarray.java
@@ -0,0 +1,24 @@
+package payilagam;
+
+public class Getdemoarray {
+
+	public static void main(String[] args) {
+	
+	int[] marks= {94,95,78,94,95,};
+	String[] subjects = {"Tam","Eng","Mat","Sci","Soc"};
+	//int count=0; 
+//	int total=0;
+for (int i=0; i<marks.length; i=i+1)
+{
+
+	if (marks[i]==94)
+  //count++;
+//  total=total+marks[i];
+{
+System.out.println( "Subjects--> "+subjects[i]);
+//System.out.println( "total: "+total); 
+	}
+	
+}
+	}
+}
