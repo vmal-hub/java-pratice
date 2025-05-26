c53a6eb1e232afabf30d26a8dd00514eb88fa0bd
vmal-hub <mohanvimal042002@gmail.com>
2025-05-26 10:55:23 +0530

    pn

---
+++ Primecheck.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Primecheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+
+        if (isPrime(num)) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1)
+            return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
+
